4f41ec70487458ddcfe882e3cc8ac05a1d389102
trixobird <george.trichopoulos@gmail.com>
2019-02-16 21:29:48 +0200

    Initial Commit

---
+++ src/main/java/pumapay/controllers/WebSocketController.java
@@ -0,0 +1,33 @@
+package pumapay.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+
+@EnableScheduling
+@Controller
+public class WebSocketController {
+
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    public WebSocketController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    private void sendMsg() {
+        template.convertAndSend("/chat", LocalDateTime.now() + " - EUR/USD: " + getRate());
+    }
+
+    private String getRate() {
+        double random = Math.random() * 0.3 + 1;
+        DecimalFormat df = new DecimalFormat("#.00");
+        return df.format(random);
+    }
+}
---
+++ src/test/java/cy/pumapay/msg/MsgApplicationTests.java
@@ -0,0 +1,17 @@
+package cy.pumapay.msg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
